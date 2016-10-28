37ad854f20ef740af515ec9993b698d947121fa9
will <will@photoshelter.com>
2016-10-28 17:30:28 -0400

    move positioning of content into mixin

--- action-hero.js
+++ action-hero.js
@@ -3,11 +3,11 @@ Polymer({
 
 	properties: {
 		/**
-		 * The position of the content, separated by a space. Permitted values are "top", "middle", "bottom" for vertical position, "left", "center" and "right" for the horizontal.
+		 * The position of the content, separated by a dash. Permitted values are "top", "middle", "bottom" for vertical position, "left", "center" and "right" for the horizontal.
 		 */
 		contentPosition: {
 			type: String,
-			value: 'center middle'
+			value: 'center-center'
 		},
 	},
 
@@ -17,12 +17,7 @@ Polymer({
 	* Life cycle function that gets called when the element is
 	* first attached to the DOM
 	*/
-	attached: function attached() {
-		var heroContent = this.shadowRoot ? this.shadowRoot.querySelector('.hero-content') :	this.querySelector('.hero-content');
-		var positions = this.contentPosition.split(' ')
-		for ( i = 0; i < positions.length; i++ ) {
-			heroContent.classList.add(positions[i]);
-		}
-	}
+	attached: function attached() {}
+
 
 });
